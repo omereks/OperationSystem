5daff9021e458b1fd73f7cee520057794eb97a4d
omereks <omereks@gmail.com>
2021-04-03 23:15:02 +0300

    jobs2

--- ex1.c
+++ ex1.c
@@ -44,7 +44,7 @@ char * lastWord(char* str){
 }
 
 void jobs(struct CommandStruct commands[100], int sizeCommands){
-    for (int i = 0; i < sizeCommands ; i++)
+    for (int i = 0; i <= sizeCommands ; i++)
     {
         if ((commands[i].DoneOrRunnig == 0) && ((commands[i].InBackground == 1)))
         {
@@ -52,8 +52,56 @@ void jobs(struct CommandStruct commands[100], int sizeCommands){
             fflush(stdout);
         }
     }
+    commands[sizeCommands].DoneOrRunnig = 1;
 }
 
+void history(struct CommandStruct commands[100], int sizeCommands){
+    for (int i = 0; i <= sizeCommands ; i++)
+    {
+        printf("%s ", commands[i].Name);
+        if (commands[i].DoneOrRunnig == 0)
+        {
+            printf("RUNNING\n");
+            fflush(stdout);
+        } else {
+            printf("DONE\n");
+            fflush(stdout);
+        }
+    }
+    commands[sizeCommands].DoneOrRunnig = 1;
+}
+
+void builtIn(struct CommandStruct commands, char * str){
+    char ** words = splitString(str);
+    printf("%s", words[strln(words)-1]);
+    /**
+    args[0]=”ls”;
+    args[1]=”-l”;
+    args[2]=NULL;
+    execv
+    */
+}
+char** splitString(char * str){
+    int j=0; 
+    int ctr=0;
+    char** newString;
+    for(int i=0;i<=(strlen(str));i++)
+    {
+        // if space or NULL found, assign NULL into newString[ctr]
+        if(str[i]==' '||str[i]=='\0')
+        {
+            newString[ctr][j]='\0';
+            ctr++;  //for next word
+            j=0;    //for next word, init index to 0
+        }
+        else
+        {
+            newString[ctr][j]=str[i];
+            j++;
+        }
+    }
+    return newString;
+}
 int main(){
 
     struct CommandStruct  commands[100];
@@ -82,18 +130,31 @@ int main(){
             commands[curCommand].InBackground = 0;
         }
 
-
+        //jobs
         if (strcmp(commands[curCommand].Name, "jobs") == 0)
         {
             jobs(commands, curCommand);
         }
+        //history
+        else if (strcmp(commands[curCommand].Name, "history") == 0)
+        {
+            history(commands, curCommand);
+        }
+        //cd
+        else if (strcmp(commands[curCommand].Name, "cd") == 0)
+        {
+            //cd(commands, curCommand);
+        }
+        else
+        {
+            builtIn(commands[curCommand], commandInput);
+        }
         
-
-
+        /**
         printf("%s\n", commands[curCommand].Name);
         printf("%d\n", commands[curCommand].DoneOrRunnig);
         printf("%d\n", commands[curCommand].InBackground);
-        
+        */
 
         curCommand++;
 
